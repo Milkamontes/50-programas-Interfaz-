9f7f04592347764df97a4c0632a68b5906bea714
Milka Guadalupe Montes Domínguez <146610747+Milkamontes@users.noreply.github.com>
2024-11-18 20:51:44 -0800

    Update Practica_26.s

--- Practica_26.s
+++ Practica_26.s
@@ -1,6 +1,7 @@
-// Autor: Milka Guadalupe Montes Domínguez
+// Autor: Milka Guadalupe Montes Domínguez 
 // Fecha: 10-11-24
 // Descripción: Realizar operaciones AND, OR, XOR a nivel de bits en ARM64
+// Asciinema: https://asciinema.org/a/690622
 
     .section .data
 num1: .word 0x3C                // Primer número (hexadecimal 3C = 60 en decimal)
@@ -24,21 +25,21 @@ _start:
     and w2, w0, w1              // w2 = w0 AND w1
     // Preparación para imprimir el resultado AND
     ldr x0, =msg_and            // Cargar el mensaje de AND
-    mov x1, w2                  // Mover el resultado de AND a x1 para imprimir
+    uxtw x1, w2                 // Extender el resultado de AND (w2) a 64 bits en x1
     bl printf                   // Llamada a printf para mostrar el resultado de AND
 
     // Realizar operación OR
     orr w2, w0, w1              // w2 = w0 OR w1
     // Preparación para imprimir el resultado OR
     ldr x0, =msg_or             // Cargar el mensaje de OR
-    mov x1, w2                  // Mover el resultado de OR a x1 para imprimir
+    uxtw x1, w2                 // Extender el resultado de OR (w2) a 64 bits en x1
     bl printf                   // Llamada a printf para mostrar el resultado de OR
 
     // Realizar operación XOR
     eor w2, w0, w1              // w2 = w0 XOR w1
     // Preparación para imprimir el resultado XOR
     ldr x0, =msg_xor            // Cargar el mensaje de XOR
-    mov x1, w2                  // Mover el resultado de XOR a x1 para imprimir
+    uxtw x1, w2                 // Extender el resultado de XOR (w2) a 64 bits en x1
     bl printf                   // Llamada a printf para mostrar el resultado de XOR
 
     // Salir del programa
